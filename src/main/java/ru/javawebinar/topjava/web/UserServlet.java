bdf0ab61947099a39c7f61bdc50f8bc44e7d06da
Nikolay Arkhipov <arkhipovnikolas@gmail.com>
2016-09-15 20:47:03 -0700

    commit

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,16 +1,18 @@
 package ru.javawebinar.topjava.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by nikolayarkhipov on 9/15/16.
+ * User: gkislin
+ * Date: 19.08.2014
  */
-public class UserServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class UserServlet extends HttpServlet {
 
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
