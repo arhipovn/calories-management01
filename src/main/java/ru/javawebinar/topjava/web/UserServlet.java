96d1ffdcf20a9d1fb937ca4a740baad6ea81285d
Nikolay Arkhipov <arkhipovnikolas@gmail.com>
2016-09-15 20:45:49 -0700

    commit

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+/**
+ * Created by nikolayarkhipov on 9/15/16.
+ */
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
