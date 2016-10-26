8a27063c1fe3657e926e43e257cc601b4df4b68d
Nikolay Arkhipov <arkhipovnikolas@gmail.com>
2016-10-25 19:55:55 -0700

    commit

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
