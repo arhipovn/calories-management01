ba79329f1f418242e818fbd7fa21beb57fb26872
Nikolay Arkhipov <arkhipovnikolas@gmail.com>
2016-11-19 19:53:44 -0800

    commit

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.SafeHtml;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -38,6 +39,7 @@ public class Meal extends BaseEntity {
 
     @Column(name = "description", nullable = false)
     @NotEmpty
+    @SafeHtml
     private String description;
 
     @Column(name = "calories", nullable = false)
