5c5e14e24a66f292ec0e45ca560789dacb3596e8
Nikolay Arkhipov <arkhipovnikolas@gmail.com>
2016-11-20 15:57:09 -0800

    commit

---
+++ hr.sh
@@ -0,0 +1,2 @@
+mvn -B -s settings.xml -DskipTests=true clean package
+java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://postgres:test@localhost:5432/topjava02" -jar target/dependency/webapp-runner.jar target/*.war
